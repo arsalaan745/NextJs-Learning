556df916daddb5650a5a615f1e8116c39316bafd
Arsalaan Ahmad <arsalaan745@gmail.com>
2025-03-20 10:54:27 +0530

    Updated the loader alignment and updated styles

--- app/user/loading.tsx
+++ app/user/loading.tsx
@@ -1,5 +1,8 @@
-export default function(){
-    return(
-        <div>loading...</div>
-    )
-}
+export default function Loader() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+      <div className="w-8 h-8 border-4 border-gray-200 border-t-blue-600 rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+  
