db914f76a38500439a626e788ae0277b72d107f7
Arsalaan Ahmad <arsalaan745@gmail.com>
2025-03-21 06:43:03 +0530

    Made landing page pretty

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,26 @@
 
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      Home Page
+    <div className="flex flex-col items-center justify-center h-screen w-screen bg-black-100">
+      <h1 className="text-3xl font-semibold text-white-800 mb-6">Todo Application</h1>
+      <div className="flex gap-4">
+        <Link
+          className="border border-indigo-600 text-indigo-600 rounded-md px-4 py-2 hover:bg-indigo-600 hover:text-white transition"
+          href="/signin"
+        >
+          Sign In
+        </Link>
+        <Link
+          className="border border-indigo-600 text-indigo-600 rounded-md px-4 py-2 hover:bg-indigo-600 hover:text-white transition"
+          href="/signUp"
+        >
+          Sign Up
+        </Link>
+      </div>
     </div>
   );
 }
+
--- app/user/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loader() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-      <div className="w-8 h-8 border-4 border-gray-200 border-t-blue-600 rounded-full animate-spin"></div>
-      </div>
-    );
-  }
-  
