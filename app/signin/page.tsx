d210e0ccb6d4bf4f4449bccbe310cad77ef69ce4
Arsalaan Ahmad <arsalaan745@gmail.com>
2025-03-21 09:19:08 +0530

    Working on BE

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,31 @@
-export default function(){
-    return (
-        <div>
-            SignIn
-        </div>
-    )
+"use client";
+
+import axios from "axios";
+
+export default function SignIn() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-dark-900">
+      <div className="border bg-white p-8 rounded-2xl flex flex-col gap-3 w-80">
+        <h2 className="text-xl font-semibold text-gray-700 text-center">Sign Up</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          className="text-gray-800 border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="text-gray-800 border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        />
+        <button
+          onClick={() => {
+            axios.post("http://localhost:3000/api/v1/signin")
+          }}
+          className="bg-indigo-600 text-white rounded-md py-2 font-medium hover:bg-indigo-700 transition"
+        >
+          SignIn
+        </button>
+      </div>
+    </div>
+  );
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,40 @@
-export default function () {
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+
+export default function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-  <div>SignUp</div>
-);
+    <div className="flex justify-center items-center h-screen bg-dark-900">
+      <div className="border bg-white p-8 rounded-2xl flex flex-col gap-3 w-80">
+        <h2 className="text-xl font-semibold text-gray-700 text-center">Sign Up</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          onChange={e=>{setUsername(e.target.value)}}
+          className="text-gray-800 border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={e=>{setPassword(e.target.value)}}
+          className="text-gray-800 border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        />
+        <button
+          onClick={() => {
+            axios.post("http://localhost:3000/api/v1/signup", {
+              username,
+              password
+            })
+          }}
+          className="bg-indigo-600 text-white rounded-md py-2 font-medium hover:bg-indigo-700 transition"
+        >
+          SignUp
+        </button>
+      </div>
+    </div>
+  );
 }
